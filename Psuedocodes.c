8950e0a44a11956d6d7f9092216cac7c4ca1b614
Shruti Mishra <96042938+Shruti-Mishra27@users.noreply.github.com>
2022-12-18 10:12:13 +0530

    Add files via upload

---
+++ Psuedocodes.c
@@ -0,0 +1,93 @@
+/*
+
+Practice Programs
+
+______33274
+
+
+1.
+Implement the following:
+
+a.
+Write a program to store the elements in 1-D array and perform the operations like searching, sorting and reversing the elements. [Menu Driven]
+
+b.
+Read the two arrays from the user and merge them and display the elements in sorted order.[Menu Driven]
+
+c.
+Write a program to perform the Matrix addition, Multiplication and Transpose Operation. [Menu Driven]
+
+
+
+
+2.
+Implement the following for Linked List:
+
+a.
+Write a program to create a single linked list and display the node elements in reverse order.
+
+b.
+Write a program to search the elements in the linked list and display the same
+
+c.
+Write a program to create double linked list and sort the elements in the linked list.
+
+
+
+3.
+Implement the following for Stack:
+
+a.
+Write a program to implement the concept of Stack with Push, Pop, Display and Exit operations.
+
+b.
+Write a program to convert an infix expression to postfix and prefix conversion.
+
+c.
+Write a program to implement Tower of Hanoi problem.
+
+
+
+
+4.
+Implement the following for Queue:
+
+a.
+Write a program to implement the concept of Queue with Insert, Delete, Display and Exit operations.
+
+b.
+Write a program to implement the concept of Circular Queue
+
+c.
+Write a program to implement the concept of Deque.
+
+
+
+
+5.
+Implement the following data structure techniques:
+
+a.
+Write a program to construct a binary search tree.
+
+b.
+Write a program to find the depth and width of the binary tree.
+
+c.
+Write a program for inorder, postorder and preorder traversal of tree
+
+
+
+
+6.
+Implement the following data structure techniques:
+
+a.
+Write a program to implement mid-square hashing method.
+
+b.
+Write a program to implement the concept of linear probing.
+
+
+
+*/
